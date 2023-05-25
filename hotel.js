0cfb1477349b8c34c2f44f67dc44b7eee466815d
kirankalluru <129094970+kirankalluru@users.noreply.github.com>
2023-05-25 13:04:17 +0530

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,8 @@
+function bookhotel() {
+    var name=document.getElementById('name').value;
+    var email=document.getElementById('email').value;
+    var checkin=document.getElementById('check-in').value;
+    var checkout=document.getElementById('check-out').value;
+    alert(`booking confirmed for:\nNAME:${name}\nEMAIL:${email} \ncheckin: ${checkin} \ncheckout:${checkout}\n have a great day sir🙏 `);
+
+}
